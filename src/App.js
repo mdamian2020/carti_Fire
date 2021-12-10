7e3f06e2e2ac718aad2b372d8a52969ae0d58373
Mihai Damian <damian49@gmail.com>
2021-12-10 11:15:50 +0200

    Pregatire salvare pe GitHub

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import ListaCarti from "./listacarti";
+import Adaug from "./adaug";
+import Container from "react-bootstrap/Container";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore"; 
+import app from "./init";
+
+const App = () => {
+  const [lista, setLista] = useState([]);
+
+  const db = getFirestore(app);  //  Se include pentru a accesa firestore
+  let listacarti = [];
+
+  const getLista = async () => {
+    listacarti = await getDocs(collection(db, "cartiCopii"));
+    let listaNoua = listacarti.docs.map((doc) => {
+      let carte = doc.data();  //  Creez un obiect nou
+      carte.src = `imagini/${carte.src}`;  //  Corectez calea
+      carte.id = doc.id;      // adaug si ID-ul (pentru "key")
+      return carte;
+    });
+    setLista(listaNoua);   //  Actualizez obiectul "state"
+  };
+
+  useEffect(() => {
+    let listaNoua = listacarti.docs.map((doc) => {
+      let carte = doc.data();  //  Creez un obiect nou
+      carte.src = `imagini/${carte.src}`;  //  Corectez calea
+      carte.id = doc.id;      // adaug si ID-ul (pentru "key")
+      return carte;
+    });
+    setLista(listaNoua);   //  Actualizez obiectul "state"
+  }, []);
+
+  const adaugCarte = async (carte) => {
+    // Adaug un nou document folosind un ID generat automat.
+    const docRef = await addDoc(collection(db, "cartiCopii"), carte);
+    getLista();  //  Reafisez lista
+    console.log("Document adaugat cu ID: ", docRef.id);
+  };
+
+  return (
+    <Container>
+      <ListaCarti listaCarti={lista} />
+      <Adaug transmit={adaugCarte} />
+    </Container>
+  );
+}
+
+export default App;
